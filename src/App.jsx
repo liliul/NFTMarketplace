65e123ac1de6886c6e9db264fd33480c579b263b
liliul <liliuestudo@gmail.com>
2022-11-18 10:34:48 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './pages/header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1 className='text-blue-800'>Ola Vite React</h1>
+    </>
+  )
+}
+
+export default App
