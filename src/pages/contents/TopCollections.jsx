7bbc2952db8159c29e020c4bedf35cfeb167092e
liliul <liliuestudo@gmail.com>
2022-11-21 14:59:38 -0300

    add: cards feitos no TopCollections

--- src/pages/contents/TopCollections.jsx
+++ src/pages/contents/TopCollections.jsx
@@ -1,39 +1,223 @@
 
 import TopCard1 from '../../assets/card1-top-collections.jpg'
+import ProfileCollec1 from '../../assets/collection-card1.jpg'
+import Verified from '../../assets/icon-verified.svg'
 
 export default function TopCollections() {
     return (
 
         <>
-            <section className="w-full h-[599px] mt-[144px]">
+            <section className="w-full h-auto mt-[144px]">
                 <div className="ml-28 flex justify-between items-center  w-[1166px]">
                     <h1 className="text-xl text-white font-fontMontserrat font-bold">Top Collections</h1>
                     <span className="text-sm font-bold text-gradient">View All</span>
                 </div>
 
-                <section className="w-[1166px] ml-28 mt-11">
-                    <article className="w-[286px] h-[367px] rounded-[31px] border ">
-                        <div className="">
-                            <img src={TopCard1} className='overflow-hidden rounded-t-[31px]' alt="Collections img" />
+                <section className="w-[1166px] h-auto  ml-28 mt-11 grid-collections">
+                    <article className="w-[90%] h-[367px] rounded-[31px] border  border-[#73E0A9]">
+                        <div className="overflow-hidden">
+                            <img src={TopCard1} className=' rounded-t-[31px]' alt="Collections img" />
                         </div>
 
                         <div className="">
-                            <div className="">
-                                <img src="" alt="" />
-                                <div className="">
-                                    <h1>World of Women Galaxy</h1>
-                                    <div className="">
-                                        <p>By  Organization Woman</p>
-                                        <img src="" alt="" />
+                            <div className="p-5 flex items-center">
+                                <img src={ProfileCollec1} className='rounded-full overflow-hidden' alt="Profile Img" />
+                                <div className="ml-2">
+                                    <h1 className='text-white font-fontMontserrat font-bold text-sm'>World of Women Galaxy</h1>
+                                    <div className="flex">
+                                        <p className='text-xs text-white mr-2'>By  Organization Woman</p>
+                                        <img src={Verified} width="11px" alt="Verified icon" />
                                     </div>
                                 </div>
                             </div>
 
-                            <hr />
+                            <hr className='hr-gradient h-[1px] w-full' />
 
-                            <div>
-                                <p>Price</p>
-                                <b>0.218 ETH</b>
+                            <div className='p-5'>
+                                <p className='text-white font-fontUbuntu text-sm'>Price</p>
+                                <b className='text-white font-fontUbuntu text-sm'>0.218 ETH</b>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article className="w-[90%] h-[367px] rounded-[31px] border  border-[#73E0A9]">
+                        <div className="overflow-hidden">
+                            <img src={TopCard1} className=' rounded-t-[31px]' alt="Collections img" />
+                        </div>
+
+                        <div className="">
+                            <div className="p-5 flex items-center">
+                                <img src={ProfileCollec1} className='rounded-full overflow-hidden' alt="Profile Img" />
+                                <div className="ml-2">
+                                    <h1 className='text-white font-fontMontserrat font-bold text-sm'>World of Women Galaxy</h1>
+                                    <div className="flex">
+                                        <p className='text-xs text-white mr-2'>By  Organization Woman</p>
+                                        <img src={Verified} width="11px" alt="Verified icon" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <hr className='hr-gradient h-[1px] w-full' />
+
+                            <div className='p-5'>
+                                <p className='text-white font-fontUbuntu text-sm'>Price</p>
+                                <b className='text-white font-fontUbuntu text-sm'>0.218 ETH</b>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article className="w-[90%] h-[367px] rounded-[31px] border  border-[#73E0A9]">
+                        <div className="overflow-hidden">
+                            <img src={TopCard1} className=' rounded-t-[31px]' alt="Collections img" />
+                        </div>
+
+                        <div className="">
+                            <div className="p-5 flex items-center">
+                                <img src={ProfileCollec1} className='rounded-full overflow-hidden' alt="Profile Img" />
+                                <div className="ml-2">
+                                    <h1 className='text-white font-fontMontserrat font-bold text-sm'>World of Women Galaxy</h1>
+                                    <div className="flex">
+                                        <p className='text-xs text-white mr-2'>By  Organization Woman</p>
+                                        <img src={Verified} width="11px" alt="Verified icon" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <hr className='hr-gradient h-[1px] w-full' />
+
+                            <div className='p-5'>
+                                <p className='text-white font-fontUbuntu text-sm'>Price</p>
+                                <b className='text-white font-fontUbuntu text-sm'>0.218 ETH</b>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article className="w-[90%] h-[367px] rounded-[31px] border  border-[#73E0A9]">
+                        <div className="overflow-hidden">
+                            <img src={TopCard1} className=' rounded-t-[31px]' alt="Collections img" />
+                        </div>
+
+                        <div className="">
+                            <div className="p-5 flex items-center">
+                                <img src={ProfileCollec1} className='rounded-full overflow-hidden' alt="Profile Img" />
+                                <div className="ml-2">
+                                    <h1 className='text-white font-fontMontserrat font-bold text-sm'>World of Women Galaxy</h1>
+                                    <div className="flex">
+                                        <p className='text-xs text-white mr-2'>By  Organization Woman</p>
+                                        <img src={Verified} width="11px" alt="Verified icon" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <hr className='hr-gradient h-[1px] w-full' />
+
+                            <div className='p-5'>
+                                <p className='text-white font-fontUbuntu text-sm'>Price</p>
+                                <b className='text-white font-fontUbuntu text-sm'>0.218 ETH</b>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article className="w-[90%] h-[367px] rounded-[31px] border  border-[#73E0A9]">
+                        <div className="overflow-hidden">
+                            <img src={TopCard1} className=' rounded-t-[31px]' alt="Collections img" />
+                        </div>
+
+                        <div className="">
+                            <div className="p-5 flex items-center">
+                                <img src={ProfileCollec1} className='rounded-full overflow-hidden' alt="Profile Img" />
+                                <div className="ml-2">
+                                    <h1 className='text-white font-fontMontserrat font-bold text-sm'>World of Women Galaxy</h1>
+                                    <div className="flex">
+                                        <p className='text-xs text-white mr-2'>By  Organization Woman</p>
+                                        <img src={Verified} width="11px" alt="Verified icon" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <hr className='hr-gradient h-[1px] w-full' />
+
+                            <div className='p-5'>
+                                <p className='text-white font-fontUbuntu text-sm'>Price</p>
+                                <b className='text-white font-fontUbuntu text-sm'>0.218 ETH</b>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article className="w-[90%] h-[367px] rounded-[31px] border  border-[#73E0A9]">
+                        <div className="overflow-hidden">
+                            <img src={TopCard1} className=' rounded-t-[31px]' alt="Collections img" />
+                        </div>
+
+                        <div className="">
+                            <div className="p-5 flex items-center">
+                                <img src={ProfileCollec1} className='rounded-full overflow-hidden' alt="Profile Img" />
+                                <div className="ml-2">
+                                    <h1 className='text-white font-fontMontserrat font-bold text-sm'>World of Women Galaxy</h1>
+                                    <div className="flex">
+                                        <p className='text-xs text-white mr-2'>By  Organization Woman</p>
+                                        <img src={Verified} width="11px" alt="Verified icon" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <hr className='hr-gradient h-[1px] w-full' />
+
+                            <div className='p-5'>
+                                <p className='text-white font-fontUbuntu text-sm'>Price</p>
+                                <b className='text-white font-fontUbuntu text-sm'>0.218 ETH</b>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article className="w-[90%] h-[367px] rounded-[31px] border  border-[#73E0A9]">
+                        <div className="overflow-hidden">
+                            <img src={TopCard1} className=' rounded-t-[31px]' alt="Collections img" />
+                        </div>
+
+                        <div className="">
+                            <div className="p-5 flex items-center">
+                                <img src={ProfileCollec1} className='rounded-full overflow-hidden' alt="Profile Img" />
+                                <div className="ml-2">
+                                    <h1 className='text-white font-fontMontserrat font-bold text-sm'>World of Women Galaxy</h1>
+                                    <div className="flex">
+                                        <p className='text-xs text-white mr-2'>By  Organization Woman</p>
+                                        <img src={Verified} width="11px" alt="Verified icon" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <hr className='hr-gradient h-[1px] w-full' />
+
+                            <div className='p-5'>
+                                <p className='text-white font-fontUbuntu text-sm'>Price</p>
+                                <b className='text-white font-fontUbuntu text-sm'>0.218 ETH</b>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article className="w-[90%] h-[367px] rounded-[31px] border  border-[#73E0A9]">
+                        <div className="overflow-hidden">
+                            <img src={TopCard1} className=' rounded-t-[31px]' alt="Collections img" />
+                        </div>
+
+                        <div className="">
+                            <div className="p-5 flex items-center">
+                                <img src={ProfileCollec1} className='rounded-full overflow-hidden' alt="Profile Img" />
+                                <div className="ml-2">
+                                    <h1 className='text-white font-fontMontserrat font-bold text-sm'>World of Women Galaxy</h1>
+                                    <div className="flex">
+                                        <p className='text-xs text-white mr-2'>By  Organization Woman</p>
+                                        <img src={Verified} width="11px" alt="Verified icon" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <hr className='hr-gradient h-[1px] w-full' />
+
+                            <div className='p-5'>
+                                <p className='text-white font-fontUbuntu text-sm'>Price</p>
+                                <b className='text-white font-fontUbuntu text-sm'>0.218 ETH</b>
                             </div>
                         </div>
                     </article>
