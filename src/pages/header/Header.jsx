d4f4a36480856009eb48b1f4187ed45a5087d81f
liliul <liliuestudo@gmail.com>
2022-11-27 16:36:56 -0300

    add: menu open clone

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -8,14 +8,14 @@ function Header() {
     const [menuOpen, setMenuOpen] = useState(false)
 
     const handleMenu = () => {
-        alert('Open Menu')
-        document.title = 'Imagem Digital'
+        
+        setMenuOpen(!menuOpen)
     }
 
     return (
         <>
             <header 
-                className="max-w-[1440px] menuOpen w-full h-20 lg:flex lg:justify-around sm:flex sm:justify-around flex items-center justify-between bg-header fixed top-0  z-50"
+                className="max-w-[1440px]  w-full h-20 lg:flex lg:justify-around sm:flex sm:justify-around flex items-center justify-between bg-header fixed top-0  z-50"
             >
                 <img src={Logo} className="xl:pl-28 " alt="Logo header" />
 
@@ -50,13 +50,51 @@ function Header() {
                 </button>
 
                 <nav 
-                    className=' xl:hidden w-[32px] h-[32px] flex flex-col gap-1 justify-center items-end p-1 '
+                    className='  xl:hidden w-[32px] h-[32px] flex flex-col gap-1 justify-center items-end p-1 '
                     onClick={handleMenu}
                 >
                     <div className='w-full h-1 bg-gradient'></div>
                     <div className='w-[70%] h-1 bg-gradient'></div>
                     <div className='w-[50%] h-1 bg-gradient'></div> 
                 </nav>
+
+                <div className={menuOpen ? "menu bb" : "close"}>
+                    <nav className='xl:hidden relative w-[500px] h-auto '>
+                        <div className='w-full h-auto z-auto flex flex-col items-center py-8 gap-16 bg-header absolute right-0 top-0'>
+                            <div className="flex gap-8 items-center sm:hidden h-9 w-96 rounded-full border-gradient">
+                                <div className=''>
+                                    <img src={Search} className='ml-10'  alt="Search icon" />
+                                </div>            
+                   
+
+                                <input 
+                                    type="search" 
+                                    className='bg-white text-sm w-96 h-4 input-bg p-1 mr-10'  
+                                    placeholder='Search items, collections, and accounts'
+                                />
+                                </div>
+
+                                <nav className='flex flex-col items-center gap-4 lg:hidden'>
+                                    <ul className='flex items-center text-white'>
+                                        <li className='mr-[40px] text-sm'>Explore</li>
+                                        <li className='mr-[40px] text-sm'>Activity</li>
+                                        <li className='mr-[40px] text-sm'>Resources</li>
+                                    </ul>
+
+                                    <div className=' border-gradient rounded-full w-24 h-[32px] flex items-center justify-center'>
+                                        <a href="#" className='text-white font-fontUbuntu text-sm'>Account
+                                        </a>
+                                    </div>
+                                </nav>
+
+                                <button className='xl:flex xl:hidden flex items-center justify-center  bg-gradient w-44 h-8 rounded-full'>
+                                    <img src={Wallet} alt="Wallet icon" />
+                                    <b className='text-white ml-4 font-fontUbuntu text-sm'>connect Wallet
+                                    </b>
+                                </button>
+                        </div>
+                    </nav>
+                </div>
             </header>
         </>
     )   
